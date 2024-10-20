218b1495b1d39e27e4b2a9e80faf44c6d298c113
Ahmad Azuddin <azuddin@users.noreply.github.com>
2024-10-20 08:48:05 +0800

    fix: handle csrf

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -12,9 +12,11 @@ export async function getServerSession() {
   const pathname = headersObj.get("x-current-path");
   const cookie =
     [
+      cookies().get("__Secure-better-auth.csrf_token")?.value,
       cookies().get("__Secure-better-auth.session_token")?.value,
+      cookies().get("better-auth.csrf_token")?.value,
       cookies().get("better-auth.session_token")?.value,
-    ].filter((c) => c && c.length > 0).length > 0;
+    ].filter((c) => c && c.length > 0).length >= 2;
   const isprotected = pathname ? isProtectedRoute(pathname) : undefined;
 
   const session = await auth.api.getSession({
@@ -23,7 +25,17 @@ export async function getServerSession() {
 
   if (!session && isprotected) {
     if (cookie) {
+      cookies().set("__Secure-better-auth.csrf_token", "", {
+        maxAge: 0,
+        httpOnly: true,
+        secure: true,
+      });
       cookies().set("__Secure-better-auth.session_token", "", {
+        maxAge: 0,
+        httpOnly: true,
+        secure: true,
+      });
+      cookies().set("better-auth.csrf_token", "", {
         maxAge: 0,
         httpOnly: true,
         secure: true,
